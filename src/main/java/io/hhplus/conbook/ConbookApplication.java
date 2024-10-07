9356647e5d36329f106cc2a4c36282c6ecebd8ca
JaewookMun <mun1103.dev@gmail.com>
2024-10-07 19:50:45 +0900

    initial commit

---
+++ src/main/java/io/hhplus/conbook/ConbookApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.conbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hhplus/conbook/ConbookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.conbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
