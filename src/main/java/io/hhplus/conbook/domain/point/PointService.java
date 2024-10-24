0cc12e9222446e9c1a490cc991b0cc4751c00399
JaewookMun <mun1103.dev@gmail.com>
2024-10-25 03:07:17 +0900

    feat: 동시성 통합 테스트
    - 동일 좌석에 대한 동시 예약 요청에 대한 테스트
    - 예상치 못한 이유로 충전(or 결제) 요청이 중복해서 올 경우 단건만 처리할 수 있도록 동시성 처리

--- src/main/java/io/hhplus/conbook/domain/point/PointService.java
+++ src/main/java/io/hhplus/conbook/domain/point/PointService.java
@@ -18,7 +18,9 @@ public class PointService {
     public UserPoint chargePoint(long userId, long amount, LocalDateTime reqTime) {
         // start Lock
         UserPoint userPoint = userPointRepository.findPointWithPessimisticLock(userId);
-        if (userPoint.getUpdatedTime().isAfter(reqTime)) throw new NotValidRequestException(ErrorCode.INVALID_POINT_REQUEST.getCode());
+        if (userPoint.getUpdatedTime().equals(reqTime)
+                || userPoint.getUpdatedTime().isAfter(reqTime))
+            throw new NotValidRequestException(ErrorCode.INVALID_POINT_REQUEST.getCode());
 
         long totalPoint = userPoint.getPoint() + amount;
         userPoint.updatePointWith(totalPoint, reqTime);
@@ -34,7 +36,9 @@ public class PointService {
     public UserPoint spendPoint(Long userId, long amount, LocalDateTime reqTime) {
         // start Lock
         UserPoint userPoint = userPointRepository.findPointWithPessimisticLock(userId);
-        if (userPoint.getUpdatedTime().isAfter(reqTime)) throw new NotValidRequestException(ErrorCode.INVALID_POINT_REQUEST.getCode());
+        if (userPoint.getUpdatedTime().equals(reqTime)
+                || userPoint.getUpdatedTime().isAfter(reqTime))
+            throw new NotValidRequestException(ErrorCode.INVALID_POINT_REQUEST.getCode());
 
         long totalPoint = userPoint.getPoint() - amount;
         if (totalPoint < 0) throw new IllegalStateException(ErrorCode.INSUFFICIENT_POINT_BALANCE.getCode());
