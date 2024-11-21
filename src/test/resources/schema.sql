2e6a685294455721aff215a3d9935e1e48da4ff1
JaewookMun <mun1103.dev@gmail.com>
2024-11-21 23:53:24 +0900

    feat: Outbox 도메인 추가 - (2)
    - sql 파일 수정 (추가 못한 항목 추가)

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -99,6 +99,17 @@ create table notification_log
     primary key (id)
 ) engine=InnoDB;
 
+create table outbox_event
+(
+    retry_count integer not null,
+    created_at  datetime(6),
+    id          bigint  not null auto_increment,
+    event_type  varchar(255),
+    payload     TEXT,
+    status      enum ('CREATED','FAILED','PUBLISHED'),
+    primary key (id)
+) engine=InnoDB;
+
 alter table if exists notification_log
     add constraint UKnotificationid unique (id);
 
