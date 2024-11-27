afeee9ca01998152c7edd54d6e21a517f96439db
JaewookMun <mun1103.dev@gmail.com>
2024-11-28 00:52:05 +0900

    refactor: 대기열 순서 조회 시 NPE 방지

--- src/test/java/io/hhplus/conbook/infra/redis/token/TokenRedisRepositoryImplTest.java
+++ src/test/java/io/hhplus/conbook/infra/redis/token/TokenRedisRepositoryImplTest.java
@@ -2,9 +2,13 @@ package io.hhplus.conbook.infra.redis.token;
 
 import io.hhplus.conbook.domain.token.Token;
 import io.hhplus.conbook.domain.token.TokenStatus;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TokenRedisRepositoryImplTest {
@@ -23,5 +27,19 @@ class TokenRedisRepositoryImplTest {
 
         boolean existing = tokenRedisRepository.existsInPass(1L, "ttt");
         System.out.println("existing? = " + existing);
+    }
+
+    @Test
+    @DisplayName("[정상]: redis position 조회 테스트")
+    void findWaitingPosition() {
+        // given
+        long concertId = 1;
+        String waitingToken = "test";
+
+        // when
+        int position = tokenRedisRepository.findPositionFor(concertId, waitingToken);
+        System.out.println("position = " + position);
+
+        assertThat(position).isGreaterThanOrEqualTo(0);
     }
 }
